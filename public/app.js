696b2616e4052d1b6fe8861fcac78c027f64c764
contreau <72581161+contreau@users.noreply.github.com>
2025-03-26 02:15:43 -0400

    frontend updates

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// ** Handler for toggling '.selected' of action buttons and their associated <section> display
+const actionButtons = document.querySelectorAll(".action-items button");
+actionButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    // remove current selection
+    document.querySelector("button.selected").classList.remove("selected");
+    document.querySelector(`section:not(section.hide)`).classList.add("hide");
+
+    // highlight new selection
+    const actionClass = event.target.dataset.action;
+    event.target.classList.add("selected");
+    document.querySelector(`section.${actionClass}`).classList.remove("hide");
+  });
+});
