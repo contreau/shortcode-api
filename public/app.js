f21631e8d6e7d608789c3f379394db2f3dc075b4
contreau <72581161+contreau@users.noreply.github.com>
2025-03-26 17:47:50 -0400

    feat: client-side url validation for shortcode creation

--- public/app.js
+++ public/app.js
@@ -5,12 +5,49 @@ const actionButtons = document.querySelectorAll(".action-items button");
 actionButtons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     // remove current selection
-    document.querySelector("button.selected").classList.remove("selected");
-    document.querySelector(`section:not(section.hide)`).classList.add("hide");
+    document.querySelector("button.selected")?.classList.remove("selected");
+    document.querySelector(`section:not(section.hide)`)?.classList.add("hide");
 
     // highlight new selection
-    const actionClass = event.target.dataset.action;
-    event.target.classList.add("selected");
-    document.querySelector(`section.${actionClass}`).classList.remove("hide");
+    const button = /** @type {HTMLButtonElement} */ (event.target);
+    const actionClass = button.dataset.action;
+    button.classList.add("selected");
+    document.querySelector(`section.${actionClass}`)?.classList.remove("hide");
   });
+});
+
+// ** Validate URL client-side
+/**
+ * @param {string} url
+ * @returns {boolean}
+ */
+function isValidURL(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+/** @type {HTMLButtonElement | null} */
+const createShortcodeSubmitButton = document.querySelector(
+  'form[method="post"] input[type="submit"]',
+);
+
+/** @type {HTMLParagraphElement | null} */
+const invalidUrlMessage = document.querySelector("p.invalid-url-message");
+
+createShortcodeSubmitButton?.addEventListener("click", (event) => {
+  const submitButton = /** @type {HTMLInputElement} */ (event.target);
+  const urlInput = /** @type {HTMLInputElement} */ (
+    submitButton?.form?.elements[0]
+  );
+  if (!isValidURL(urlInput.value)) {
+    event.preventDefault();
+    invalidUrlMessage?.classList.add("visible");
+    setTimeout(() => {
+      invalidUrlMessage?.classList.remove("visible");
+    }, 3000);
+  }
 });
